67e0730b5efe20e99fe71395e87b9da9ca8bf15f
huiwonn <dhc5512@daum.net>
2025-07-09 15:14:25 +0900

    실습

--- 3일차/main.dart
+++ 3일차/main.dart
@@ -1,7 +1,17 @@
-void main() {
-  int temp = 3;
-  int? nullTemp;
+class Integer {
+  late int _value;
 
-  print(temp);
-  print(nullTemp);
+  Integer([int givenvalue = 0]) {
+    _value = givenvalue;
+  }
+}
+
+class TimemachineeInteger extends Integer {}
+
+void main() {
+  var int1 = Integer();
+  var int2 = Integer(3);
+
+  print(int1._value);
+  print(int2._value);
 }
