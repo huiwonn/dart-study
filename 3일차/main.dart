345b31f87f2a1d5f08a82302fb2580219bc1178e
huiwonn <dhc5512@daum.net>
2025-07-09 14:49:23 +0900

    교재실습

--- 3일차/main.dart
+++ 3일차/main.dart
@@ -1,15 +1,7 @@
 void main() {
-  List list = [1, 2, 3];
+  int temp = 3;
+  int? nullTemp;
 
-  if (list.isEmpty) {
-    print("리스트가 비어있어요.");
-  } else {
-    print("리스트가 안비어있어요");
-  }
-
-  list.isEmpty
-      ? print("리스트가 비어있어요.")
-      : list.first == 1
-      ? print("첫번째값은 1입니다.")
-      : print("첫번째값은 1이 아닙니다.");
+  print(temp);
+  print(nullTemp);
 }
