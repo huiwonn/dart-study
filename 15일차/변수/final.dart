b8f33b3f1a565d0fa39fe8eb8173447e84d4114f
huiwonn <dhc5512@daum.net>
2025-07-25 11:39:58 +0900

    수정

--- 15일차/변수/final.dart
+++ 15일차/변수/final.dart
@@ -1,17 +1,17 @@
 void main() {
-  var name = 'nico';
-  String name2 = 'nico2';
+  var name = "nico";
+  String name2 = "nico2";
 
-  //기본적으로 var 또는 다른 타입으로 생성된 변수는 계속 수정가능
+  // 기본적으로 var 또는 다른 타입으로 생성된 변수는 계속해서 수정이 가능합니다.
   name = "hi";
-  name2 = 'hello';
+  name2 = "hello";
 
-  //수정을 할 수 없는 변수를 만들어야 할 때 final이라는 자료형 사용
-  final name3 = 'helllllo';
+  // 수정을 할 수 없는 변수를 만들어야 할 때 저희는 final 이라는 자료형을 사용하면 됩니다.
+  final name3 = "HELLLLO";
 
-  name3 = 'hihi';
-  //final 자료형은 단 한 번 자료형 지정하고 난 후에는 수정 불가능
+  name3 = "HIIIIIIIIIII";
+  // final 자료형은 단 한 번 변수의 값을 지정하고 난 후에는 추가적인 수정이 불가능합니다.
 
-  //final 자료형은 변수의 타입도 지정할 수 있다
+  // final 자료형은 변수의 타입도 지정을 해줄 수 있습니다.
   final String name4;
 }
